728fcd2ca88de3d83e7973efc1e2c9c122d9c7f6
Ruben Castro <rubenjose@hotmail.com>
2022-09-08 23:31:13 -0400

    Adding Edit and Delete Feature

---
+++ src/app/helpers/people.model.ts
@@ -0,0 +1,13 @@
+export interface PeopleModel {
+    name?: string ,
+    last_name?: string,
+    email?: string,
+    phone?: string,
+    age?: string,
+    refered_by?: string,
+    church_from?: string,
+    emergency_contract?: string,
+    relationship?: string,
+    translation?: string,
+    emergency_phone?: string
+}
