5239b59538b3d25b69d0002ecffa54e0593a9342
Ruben Castro <rubenjose@hotmail.com>
2022-09-19 18:48:57 -0400

    adding css improvements to the Menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,10 +8,41 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
-  
+  collapse:boolean = true;
   constructor(private authService: AuthService, private route: Router) { }
 
   ngOnInit(): void {
+  }
+
+  menuItems = [
+    {
+      name: 'Home',
+      route: '',
+      icon: 'fa-solid fa-house'
+    },
+    {
+      name: 'Users',
+      route:'users',
+      icon:'fa-solid fa-users'
+    },
+    {
+      name: 'Encuentros',
+      icon: 'fa-solid fa-hands-praying'
+    },
+    {
+      name: 'NewLife',
+      icon: 'fa-solid fa-dove'
+    },
+    {
+      name: 'Logout',
+      icon: 'fa-solid fa-right-from-bracket'
+    }
+
+  ]
+
+  openCollapse(event:any){
+    this.collapse = !this.collapse;
+    console.log(this.collapse);
   }
 
   logOut(){
